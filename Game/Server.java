7e0fa6c4f8d8e8645136782edc5c1ad307b3ee71
JonathanEdmund <jonathan.edmund@gmail.com>
2022-11-16 22:39:28 +0700

    create client and server socket connection

---
+++ Game/Server.java
@@ -0,0 +1,29 @@
+package Game;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(3000);
+        Socket socket = null;
+        
+        while(true) {
+            // listening for client 1
+            System.out.println("waiting for client 1...");
+            socket = serverSocket.accept();
+            Client c1 = new Client(socket);
+            System.out.println("client 1 joined...");
+
+            // listening for client 2
+            System.out.println("waiting for client 2...");
+            socket = serverSocket.accept();
+            Client c2 = new Client(socket);
+            System.out.println("client 2 joined...");
+
+            // start game
+            System.out.println("game started");
+        }
+    }
+}
